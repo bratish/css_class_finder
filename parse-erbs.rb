9ee8ffea184108d32d071086a23a5e4671d94351
Bratish Goswami <bgoswami@crri.co.in>
2011-03-22 17:09:58 +0530

    regex modified

--- parse-erbs.rb
+++ parse-erbs.rb
@@ -4,5 +4,6 @@ require 'pp'
 file = 'index.html.erb'
 content = File.open(file, 'r').read
 
-x = content.grep(/[:]?class[ ]?=[ >]?/).map{|line| line.strip! }
+x = content.grep(/[:]?class[ ]?=[ >]?/).map{|line| line.strip!.match(/[:]?class[ ]?=[ >]?[ ]*[\\]?["|']([\w <%=\->:,'"\.\(\)#{}]*)[ ]*[\\]?["|']/)[1]}
+pp x
 
