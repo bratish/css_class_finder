e5b212e7e2b32884d7df0b64f474eb48f71998de
Bratish Goswami <bgoswami@crri.co.in>
2011-03-23 14:58:51 +0530

    regex modified

--- parse-erbs.rb
+++ parse-erbs.rb
@@ -4,6 +4,11 @@ require 'pp'
 file = 'index.html.erb'
 content = File.open(file, 'r').read
 
-x = content.grep(/[:]?class[ ]?=[ >]?/).map{|line| line.strip!.match(/[:]?class[ ]?=[ >]?[ ]*[\\]?["|']([\w <%=\->:,'"\.\(\)#{}]*)[ ]*[\\]?["|']/)[1]}
-pp x
+x = content.
+      grep(/[:]?class[ ]?=[ >]?/).
+        map{|line| line.strip!.match(/[:]?class[ ]?=[ >]?[ ]*[\\]?["|']([\w <%=\->:,?@'"\.\(\)#{}]*)[ ]*[\\]?["|']/)[1]}
+
+x.each do|str|
+  p str.scan(/(<%[\w =\-:,?@'"\.\(\)#{}]*%>)/)
+end
 
