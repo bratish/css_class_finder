3f6f00dc15ea28afa3bca65414aeb510f53452e9
Bratish Goswami <bgoswami@crri.co.in>
2011-04-04 17:58:45 +0530

    testing

--- test.rb
+++ test.rb
@@ -6,16 +6,23 @@ a = ["fLeft <%= @disable_child ? 'AddChildtabs' : 'tabs' -%> clearFix <%= 'blah'
  "<%= 'selectedBlue' if @topic and @topic.id == topic.id %>",
  "width16 pRight5 mLeft10 txt<%= student_assessment.status_to_sting.gsub(' ', '') -%> txtBold"]
 
-p a.first.index("<%")
-p a.first.index("%>")
-s = a.first
+#p a.first.index("<%")
+#p a.first.index("%>")
+s = a[1]
+p s
 interpolate_arr = []
 rest_of_the_str = ''
 
 loop do
+  p "loop"
   start_index = s.index("<%")
   end_index = s.index("%>")
   break if start_index.nil? or end_index.nil?
-
+  rest_of_the_str += s[0..start_index-1]
+  interpolate_arr.push(s[start_index +2..end_index-1])
+  s = s[end_index + 2..s.size]
 end
+
+p interpolate_arr
+p rest_of_the_str
 
