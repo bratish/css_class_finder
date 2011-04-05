b40290a49435d26956aff417c51c4f7a650160dc
Bratish Goswami <bgoswami@crri.co.in>
2011-04-05 17:18:12 +0530

    testing

--- test.rb
+++ test.rb
@@ -1,34 +1,38 @@
 require 'rubygems'
 require 'pp'
 
-a = ["fLeft <%= @disable_child ? 'AddChildtabs' : 'tabs' -%> clearFix <%= 'blah' %>",
+a = ["fLeft <%= @disable_child ? 'AddChildtabs' : 'tabs' : \'bad\"  -%> clearFix <%= 'blah' %>",
  "<%= 'selectedBlue' if @topic.blank? %>",
  "<%= 'selectedBlue' if @topic and @topic.id == topic.id %>",
- "width16 pRight5 mLeft10 txt<%= student_assessment.status_to_sting.gsub(' ', '') -%> txtBold"]
+ "width16 pRight5 mLeft10 txt<%= student_assessment.status_to_sting.gsub(' ', '') -%> txtBold",
+ "width16 pRight5 mLeft10 txt#{"student_assessment.status_to_sting.gsub(' ', '')"} txtBold"
+ ]
 
-#p a.first.index("<%")
-#p a.first.index("%>")
-s = a[1]
-p s
+s = "#{a[0] } <% %>"
 interpolate_arr = []
 rest_of_the_str = ''
+partial_class_references = []
 
 loop do
-#  p "loop"
   start_index = s.index("<%")
   end_index = s.index("%>")
-  p start_index
-  p end_index
-  p interpolate_arr
-  p rest_of_the_str
+  partial_class_reference = ""
   break if s.strip.size == 0 or start_index.nil? or end_index.nil?
-#  p "loop 2"
-  rest_of_the_str += s[0..start_index-1] if start_index-1 > -1
-  interpolate_arr.push(s[start_index +2..end_index-1]) # if start_index-1 > -1 and end_index > -1
+  if (start_index-1) > 0 and s[start_index-1].chr != " "
+    partial_class_reference = s[0..start_index-1].split(" ").last
+    partial_class_references.push(partial_class_reference)
+  end
+  p s[0..start_index-1]
+  rest_of_the_str += s[0..start_index-1].gsub!(partial_class_reference, "") if start_index-1 > -1
+  interpolate_arr.push(s[start_index +2..end_index-1].scan(/'([\w]+)'|"([\w]+)"/)) # if start_index-1 > -1 and end_index > -1
   s = s[end_index + 2..s.size]
+  break if s.strip.size == 0 
 end
 
+pure_class_references = rest_of_the_str.strip.split(" ").uniq
 
 
-  p interpolate_arr
-  p rest_of_the_str
+  p interpolate_arr.flatten.compact
+  p pure_class_references
+  p partial_class_references
+
