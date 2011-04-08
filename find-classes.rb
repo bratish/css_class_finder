56dcd2a1e9cf374a1f06b15dcae789694574f25e
Bratish Goswami <bgoswami@crri.co.in>
2011-04-08 17:55:51 +0530

    main stuff modified

--- find-classes.rb
+++ find-classes.rb
@@ -6,35 +6,49 @@ require 'util'
 
 include CssParser
 
-def doubtful_entry(css_class)
+railsroot = "/Users/bratish/solaro"
+class_hash = {}
+
+Dir.glob(File.join(railsroot, "public", "stylesheets", "*.css")) do |f|
+  parser = CssParser::Parser.new
+  parser.load_file!(f)
+  array_of_classes = []
   
+  parser.each_selector do |selector, declarations, specificity|
+    array_of_classes.push(selector) if selector.include? "."
+  end
+  class_hash[f] = array_of_classes.collect{|x| x.scan(/\.([\w\-]+)/)}.flatten.uniq!
 end
-
-file = "application.css"
-parser = CssParser::Parser.new
-
-parser.load_file!(file)
-array_of_classes = []
-
-parser.each_selector do |selector, declarations, specificity|
-  array_of_classes.push(selector) if selector.include? "."
-end
+pp class_hash;exit
+#file = "application.css"
+#parser = CssParser::Parser.new
+#
+#parser.load_file!(file)
+#array_of_classes = []
+#
+#parser.each_selector do |selector, declarations, specificity|
+#  array_of_classes.push(selector) if selector.include? "."
+#end
 
 classes_in_file = array_of_classes.collect{|x| x.scan(/\.([\w\-]+)/)}.flatten.uniq!
 f = 'index.html.erb'
 pcc = PickCssClasses.new(f)
 clean_class_references = pcc.pure_class_references
-class_references_with_script_tags = pcc.class_references_with_script_tags
-pp class_references_with_script_tags; exit
+class_references_inside_script_tags = pcc.dynamic_class_references.flatten.compact.uniq
+pp class_references_inside_script_tags
+pp pcc.partial_class_references
+#pp class_references_with_script_tags; exit
 classes_found_clean = []
-classes_found_with_doubt = []
+dynamic_class_references = []
 puts "#{f}:"
 classes_in_file.each do |c|
-
-classes_found_clean.push(c) if clean_class_references.include?(c)
-
-  puts "    #{c}" if class_references_with_script_tags.include?(c)
+  classes_found_clean.push(c) if clean_class_references.include?(c)
+#  puts "    #{c}" if class_references_with_script_tags.include?(c)
+  dynamic_class_references.push(c) if class_references_inside_script_tags.include?(c)
 end
+
+pp classes_found_clean
+pp dynamic_class_references
 #  puts "  Cleanly found:"
 #  #  puts "  Cleanly found:"
 #puts "    #{c}"
--- pick_css_classes.rb
+++ pick_css_classes.rb
@@ -1,5 +1,5 @@
-require 'rubygems'
-require 'pp'
+#require 'rubygems'
+#require 'pp'
 
 class PickCssClasses
   attr_accessor :class_references, :class_references_with_script_tags, :pure_class_references, :partial_class_references, :dynamic_class_references
@@ -23,27 +23,27 @@ class PickCssClasses
     @class_references_with_script_tags.each do |s|
       interpolate_arr = s.scan(/#\{([a-zA-Z0-9_\.'\(\) ,\?:"]*)\}/).flatten
       script_tag_arr = s.scan(/<%([\w =\-:,?@'"\.\(\)#\{\}]*)%>/).flatten
-      @pure_class_references.push(s.gsub(/[a-zA-Z0-9_]*<%[=]?[ ]*[\w =\-:,?@'"\.\(\)#\{\}]*[ ]*[\-]?%>/, "").gsub(/[a-zA-Z0-9_]*#\{[a-zA-Z0-9_\.'\(\) ,\?:"]*\}/, "").split(" ").compact.uniq)
+      @pure_class_references.push(s.gsub(/[a-zA-Z0-9_]*<%[=]?[ ]*[\w =\-:,?@'"\.\(\)#\{\}]*[ ]*[\-]?%>/, "").gsub(/[a-zA-Z0-9_]*#\{[a-zA-Z0-9_\.'\(\) ,\?:"]*\}/, "").split(" ").compact.uniq).flatten.uniq
       @partial_class_references = (s.scan(/([a-zA-Z0-9]+)#\{[a-zA-Z0-9_\.'\(\) ,\?:"]*\}/)+ s.scan(/([a-zA-Z0-9]+)<%[\w =\-:,?@'"\.\(\)#\{\}]*%>/)).flatten
       class_references_inside_interpolations = (interpolate_arr.collect{|elem| elem.scan(/'([\w]+)'/)} + interpolate_arr.collect{|elem| elem.scan(/"([\w]+)"/)}).flatten.compact.uniq
       class_references_inside_script_tags = (script_tag_arr.collect{|elem| elem.scan(/'([\w]+)'/)} + script_tag_arr.collect{|elem| elem.scan(/"([\w]+)"/)}).flatten.compact.uniq
-      @dynamic_class_references.push(class_references_inside_interpolations + class_references_inside_script_tags)
+      @dynamic_class_references.push((class_references_inside_interpolations + class_references_inside_script_tags))
     end
   end
 end
 
-pcc = PickCssClasses.new('index.html.erb')
+#pcc = PickCssClasses.new('index.html.erb')
 #p "class_references"
 #pp pcc.class_references
 #p "========================="
 #p "class_references_with_script_tags"
 #pp pcc.class_references_with_script_tags
-p "========================="
-p "pure_class_references"
-pp pcc.pure_class_references.flatten.uniq
-p "========================="
-p "partial_class_references"
-pp pcc.partial_class_references
-p "========================="
-p "dynamic_class_references"
-pp pcc.dynamic_class_references.flatten.compact.uniq
+#p "========================="
+#p "pure_class_references"
+#pp pcc.pure_class_references.flatten.uniq
+#p "========================="
+#p "partial_class_references"
+#pp pcc.partial_class_references
+#p "========================="
+#p "dynamic_class_references"
+#pp pcc.dynamic_class_references.flatten.compact.uniq
